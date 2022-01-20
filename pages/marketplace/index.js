92609ad8a3165231572947a723b61dba81863134
Sotatek-NguyenNguyen4 <93501310+Sotatek-NguyenNguyen4@users.noreply.github.com>
2022-01-21 01:13:45 +0700

    eth price

--- pages/marketplace/index.js
+++ pages/marketplace/index.js
@@ -3,16 +3,18 @@
 import { CourseList, CourseCard } from "@components/ui/course"
 import { BaseLayout } from "@components/ui/layout"
 import { getAllCourses } from "@content/courses/fetcher"
-import { WalletBar } from "@components/ui/web3"
+import { EthRates, WalletBar } from "@components/ui/web3"
 import { useAccount, useNetwork } from "@components/hooks/web3"
 import { Button } from "@components/ui/common"
 import { OrderModal } from "@components/ui/order"
 import { useState } from "react"
+import { useEthPrice } from "@components/hooks/useEthPrice"
 
 export default function Marketplace({ courses }) {
   const [selectedCourse, setSelectedCourse] = useState(null)
   const { account } = useAccount()
   const { network } = useNetwork()
+  const { eth } = useEthPrice()
 
   return (
     <>
@@ -28,6 +30,9 @@ export default function Marketplace({ courses }) {
         />
 
       </div>
+      <EthRates
+        eth={eth.data}
+      />
       <CourseList
         courses={courses}
       >
