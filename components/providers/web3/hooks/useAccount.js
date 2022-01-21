230c20e67ca1de903accc2fe646f6c931f243404
Sotatek-NguyenNguyen4 <93501310+Sotatek-NguyenNguyen4@users.noreply.github.com>
2022-01-22 02:20:53 +0700

    One time event

--- components/providers/web3/hooks/useAccount.js
+++ components/providers/web3/hooks/useAccount.js
@@ -23,9 +23,14 @@ export const handler = (web3, provider) => () => {
   )
 
   useEffect(() => {
-    provider && provider.on("accountsChanged",
-      accounts => mutate(accounts[0] ?? null)
-    )
+    const mutator = accounts => mutate(accounts[0] ?? null)
+
+    provider && provider.on("accountsChanged", mutator)
+
+    return () => {
+      provider?.removeListener("accountsChanged", mutator)
+    }
+
   }, [provider])
 
   return {
