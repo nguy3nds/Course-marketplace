4976859eff2d67c27a77b562b9e68483d908a091
Sotatek-NguyenNguyen4 <93501310+Sotatek-NguyenNguyen4@users.noreply.github.com>
2022-01-22 01:18:16 +0700

    Course state message

--- pages/courses/[slug].js
+++ pages/courses/[slug].js
@@ -1,5 +1,5 @@
 import { useAccount, useOwnedCourse } from "@components/hooks/web3";
-import { Modal } from "@components/ui/common";
+import { Modal, Message } from "@components/ui/common";
 import {
   CourseHero,
   Curriculum,
@@ -11,13 +11,14 @@ import { getAllCourses } from "@content/courses/fetcher";
 export default function Course({ course }) {
   const { account } = useAccount()
   const { ownedCourse } = useOwnedCourse(course, account.data)
-  console.log(ownedCourse)
+  const courseState = ownedCourse.data?.state
+  // const courseState = "deactivated"
 
   return (
     <>
-      {course.title}
       <div className="py-4">
         <CourseHero
+          hasOwner={!!ownedCourse.data}
           title={course.title}
           description={course.description}
           image={course.coverImage} />
@@ -25,6 +26,28 @@ export default function Course({ course }) {
       <Keypoints
         points={course.wsl}
       />
+      {courseState &&
+        <div className="max-w-5xl mx-auto">
+          {courseState === "purchased" &&
+            <Message type="warning">
+              Course is purchased and waiting for the activation. Process can take up to 24 hours.
+              <i className="block font-normal">In case of any questions, please contact info@eincode.com</i>
+            </Message>
+          }
+          {courseState === "activated" &&
+            <Message type="success">
+              Eincode wishes you happy watching of the course.
+            </Message>
+          }
+          {courseState === "deactivated" &&
+            <Message type="danger">
+              Course has been deactivated, due the incorrect purchase data.
+              The functionality to watch the course has been temporaly disabled.
+              <i className="block font-normal">Please contact info@eincode.com</i>
+            </Message>
+          }
+        </div>
+      }
       <Curriculum
         locked={true}
       />
