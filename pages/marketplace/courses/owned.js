ca2f96669953aa60afff35cb7439df2b6c378836
Sotatek-NguyenNguyen4 <93501310+Sotatek-NguyenNguyen4@users.noreply.github.com>
2022-01-22 01:55:23 +0700

    isEmpty and empty account

--- pages/marketplace/courses/owned.js
+++ pages/marketplace/courses/owned.js
@@ -1,21 +1,48 @@
 import { useAccount, useOwnedCourses } from "@components/hooks/web3";
+import { useWeb3 } from "@components/providers";
 import { Button, Message } from "@components/ui/common";
 import { OwnedCourseCard } from "@components/ui/course";
 import { BaseLayout } from "@components/ui/layout";
 import { MarketHeader } from "@components/ui/marketplace";
 import { getAllCourses } from "@content/courses/fetcher";
+import Link from "next/link";
 
 export default function OwnedCourses({ courses }) {
   const { account } = useAccount();
   const { ownedCourses } = useOwnedCourses(courses, account.data);
-
-  console.log(ownedCourses);
+  const { requireInstall } = useWeb3()
 
   return (
     <>
       <MarketHeader />
 
       <section className="grid grid-cols-1">
+        {ownedCourses.isEmpty &&
+          <div className="w-1/2">
+            <Message type="warning">
+              <div>You don't own any courses</div>
+              <Link href="/marketplace">
+                <a className="font-normal hover:underline">
+                  <i>Purchase Course</i>
+                </a>
+              </Link>
+            </Message>
+          </div>
+        }
+        {account.isEmpty &&
+          <div className="w-1/2">
+            <Message type="warning">
+              <div>Please connect to Metamask</div>
+            </Message>
+          </div>
+        }
+        {requireInstall &&
+          <div className="w-1/2">
+            <Message type="warning">
+              <div>Please install Metamask</div>
+            </Message>
+          </div>
+        }
         {ownedCourses.data?.map(course =>
           <OwnedCourseCard
             key={course.id}
