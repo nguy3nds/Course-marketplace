06b94f30bc2131af93ae597ad22afd2f1391ed89
Sotatek-NguyenNguyen4 <93501310+Sotatek-NguyenNguyen4@users.noreply.github.com>
2022-01-21 22:54:29 +0700

    Wallet bar improvement

--- components/ui/web3/walletbar/index.js
+++ components/ui/web3/walletbar/index.js
@@ -1,5 +1,7 @@
+
 import { useWalletInfo } from "@components/hooks/web3"
 import { useWeb3 } from "@components/providers"
+import { Button } from "@components/ui/common"
 
 
 export default function WalletBar() {
@@ -9,40 +11,39 @@ export default function WalletBar() {
   return (
     <section className="text-white bg-indigo-600 rounded-lg">
       <div className="p-8">
-        <h1 className="text-2xl">Hello, {account.data}</h1>
-        <h2 className="subtitle mb-5 text-xl">I hope you are having a great day!</h2>
+        <h1 className="text-base xs:text-xl break-words">Hello, {account.data}</h1>
+        <h2 className="subtitle mb-5 text-sm xs:text-base">I hope you are having a great day!</h2>
         <div className="flex justify-between items-center">
           <div className="sm:flex sm:justify-center lg:justify-start">
-            <div className="rounded-md shadow">
-              <a href="#" className="w-full flex items-center justify-center px-8 py-3 border border-transparent text-base font-medium rounded-md text-black bg-white hover:bg-gray-100 md:py-4 md:text-lg md:px-10">
-                Learn how to purchase
-              </a>
-            </div>
+            <Button
+              className="mr-2 text-sm xs:text-lg p-2"
+              variant="white">
+              Learn how to purchase
+            </Button>
           </div>
           <div>
-            <div>
-              {network.hasInitialResponse && !network.isSupported &&
-                <div className="bg-red-400 p-4 rounded-lg">
-                  <div>Connected to wrong network</div>
-                  <div>
-                    Connect to: {` `}
-                    <strong className="text-2xl">
-                      {network.target}
-                    </strong>
-                  </div>
-                </div>
-              }
-              {requireInstall &&
-                <div className="bg-yellow-500 p-4 rounded-lg">
-                  Cannot connect to network. Please install Metamask.
-                </div>
-              }
-              {network.data &&
+            {network.hasInitialResponse && !network.isSupported &&
+              <div className="bg-red-400 p-4 rounded-lg">
+                <div>Connected to wrong network</div>
                 <div>
-                  <span>Currently on </span>
-                  <strong className="text-2xl">{network.data}</strong>
+                  Connect to: {` `}
+                  <strong className="text-2xl">
+                    {network.target}
+                  </strong>
                 </div>
-              }</div>
+              </div>
+            }
+            {requireInstall &&
+              <div className="bg-yellow-500 p-4 rounded-lg">
+                Cannot connect to network. Please install Metamask.
+              </div>
+            }
+            {network.data &&
+              <div>
+                <span>Currently on </span>
+                <strong className="text-2xl">{network.data}</strong>
+              </div>
+            }
           </div>
         </div>
       </div>
