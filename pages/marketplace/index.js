31403992b4e906d19e640219e259bfc44be9bfb4
Sotatek-NguyenNguyen4 <93501310+Sotatek-NguyenNguyen4@users.noreply.github.com>
2022-01-23 01:11:24 +0700

    Repurchase

--- pages/marketplace/index.js
+++ pages/marketplace/index.js
@@ -14,7 +14,9 @@ export default function Marketplace({ courses }) {
   const { web3, contract, requireInstall } = useWeb3()
   const { hasConnectedWallet, isConnecting, account } = useWalletInfo()
   const { ownedCourses } = useOwnedCourses(courses, account.data)
+
   const [selectedCourse, setSelectedCourse] = useState(null)
+  const [isNewPurchase, setIsNewPurchase] = useState(true)
 
   const purchaseCourse = async order => {
     const hexCourseId = web3.utils.utf8ToHex(selectedCourse.id)
@@ -22,18 +24,38 @@ export default function Marketplace({ courses }) {
       { type: "bytes16", value: hexCourseId },
       { type: "address", value: account.data }
     )
-    const emailHash = web3.utils.sha3(order.email)
-    const proof = web3.utils.soliditySha3(
-      { type: "bytes32", value: emailHash },
-      { type: "bytes32", value: orderHash }
-    )
 
     const value = web3.utils.toWei(String(order.price))
 
+    if (isNewPurchase) {
+      const emailHash = web3.utils.sha3(order.email)
+      const proof = web3.utils.soliditySha3(
+        { type: "bytes32", value: emailHash },
+        { type: "bytes32", value: orderHash }
+      )
+
+      _purchaseCourse(hexCourseId, proof, value)
+    } else {
+      _repurchaseCourse(orderHash, value)
+    }
+  }
+
+  const _purchaseCourse = async (hexCourseId, proof, value) => {
     try {
       const result = await contract.methods.purchaseCourse(
         hexCourseId,
         proof
+      ).send({ from: account.data, value })
+      console.log(result)
+    } catch {
+      console.error("Purchase course: Operation has failed.")
+    }
+  }
+
+  const _repurchaseCourse = async (courseHash, value) => {
+    try {
+      const result = await contract.methods.repurchaseCourse(
+        courseHash
       ).send({ from: account.data, value })
       console.log(result)
     } catch {
@@ -47,98 +69,99 @@ export default function Marketplace({ courses }) {
       <CourseList
         courses={courses}
       >
-        {course =>
-          <CourseCard
-            key={course.id}
-            course={course}
-            disabled={!hasConnectedWallet}
-            Footer={() => {
-              if (requireInstall) {
-                return (
-                  <Button
-                    disabled={true}
-                    variant="lightPurple">
-                    Install
-                  </Button>
-                )
-              }
+        {course => {
+          const owned = ownedCourses.lookup[course.id]
+          return (
+            <CourseCard
+              key={course.id}
+              course={course}
+              state={owned?.state}
+              disabled={!hasConnectedWallet}
+              Footer={() => {
+                if (requireInstall) {
+                  return (
+                    <Button
+                      size="sm"
+                      disabled={true}
+                      variant="lightPurple">
+                      Install
+                    </Button>
+                  )
+                }
 
-              if (isConnecting) {
-                return (
-                  <Button
-                    disabled={true}
-                    variant="lightPurple">
-                    <Loader size="sm" />
-                  </Button>
-                )
-              }
+                if (isConnecting) {
+                  return (
+                    <Button
+                      size="sm"
+                      disabled={true}
+                      variant="lightPurple">
+                      <Loader size="sm" />
+                    </Button>
+                  )
+                }
 
-              if (!ownedCourses.hasInitialResponse) {
-                return (
-                  <div style={{ height: "50px" }}></div>
-                )
-              }
+                if (!ownedCourses.hasInitialResponse) {
+                  return (
+                    <div style={{ height: "42px" }}></div>
+                  )
+                }
 
-              const owned = ownedCourses.lookup[course.id]
-
-              if (owned) {
-                return (
-                  <>
-                    <div>
-                      <Button
-                        disabled={true}
-                        variant="green">
-                        Owned
-                      </Button>
-                      {owned.state === "deactivated" &&
+                if (owned) {
+                  return (
+                    <>
+                      <div className="flex">
                         <Button
+                          onClick={() => alert("You are owner of this course.")}
                           disabled={false}
-                          onClick={() => alert("Re-activating")}
-                          variant="purple">
-                          Fund to Activate
+                          size="sm"
+                          variant="white">
+                          Yours &#10004;
                         </Button>
-                      }
-                    </div>
-                    <div className="mt-1">
-                      {owned.state === "activated" &&
-                        <Message size="sm">
-                          Activated
-                        </Message>
-                      }
-                      {owned.state === "deactivated" &&
-                        <Message type="danger" size="sm">
-                          Deactivated
-                        </Message>
-                      }
-                      {owned.state === "purchased" &&
-                        <Message type="warning" size="sm">
-                          Waiting for Activation
-                        </Message>
-                      }
-                    </div>
-                  </>
+                        {owned.state === "deactivated" &&
+                          <div className="ml-1">
+                            <Button
+                              size="sm"
+                              disabled={false}
+                              onClick={() => {
+                                setIsNewPurchase(false)
+                                setSelectedCourse(course)
+                              }}
+                              variant="purple">
+                              Fund to Activate
+                            </Button>
+                          </div>
+                        }
+                      </div>
+                    </>
+                  )
+                }
+
+
+                return (
+                  <Button
+                    onClick={() => setSelectedCourse(course)}
+                    size="sm"
+                    disabled={!hasConnectedWallet}
+                    variant="lightPurple">
+                    Purchase
+                  </Button>
                 )
               }
-
-
-              return (
-                <Button
-                  onClick={() => setSelectedCourse(course)}
-                  disabled={!hasConnectedWallet}
-                  variant="lightPurple">
-                  Purchase
-                </Button>
-              )
-            }
-            }
-          />
+              }
+            />
+          )
+        }
         }
       </CourseList>
       {selectedCourse &&
         <OrderModal
           course={selectedCourse}
+          isNewPurchase={isNewPurchase}
           onSubmit={purchaseCourse}
-          onClose={() => setSelectedCourse(null)}
+          onClose={() => {
+            setSelectedCourse(null)
+            setIsNewPurchase(true)
+          }}
         />
       }
     </>
