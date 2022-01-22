718ab68467cf57d3c94066f8f9c58914986354c2
Sotatek-NguyenNguyen4 <93501310+Sotatek-NguyenNguyen4@users.noreply.github.com>
2022-01-23 01:32:30 +0700

    Search and filter

--- components/ui/course/card/Managed.js
+++ components/ui/course/card/Managed.js
@@ -3,7 +3,7 @@
 const Item = ({ title, value, className }) => {
 
   return (
-    <div className={`${className} px-4 py-2  sm:px-6`}>
+    <div className={`${className} px-4 py-2 sm:px-6`}>
       <div className="text-sm font-medium text-gray-500">
         {title}
       </div>
@@ -15,23 +15,20 @@ const Item = ({ title, value, className }) => {
 }
 
 
-export default function ManagedCourseCard({ children, course }) {
+export default function ManagedCourseCard({ children, course, isSearched = false }) {
 
   return (
-    <div className="bg-white border shadow overflow-hidden sm:rounded-lg mb-3">
-      <div className="border-t border-gray-200">
-        {Object.keys(course).map((key, i) =>
-          <Item
-            key={key}
-            className={`${i % 2 ? "bg-gray-50" : "bg-white"}`}
-            // title={key}
-            title={key[0].toUpperCase() + key.slice(1)}
-            value={course[key]}
-          />
-        )}
-        <div className="bg-white px-4 py-5 sm:px-6">
-          {children}
-        </div>
+    <div className={`${isSearched ? "border-indigo-600" : "bg-gray-200"} bg-white border shadow overflow-hidden sm:rounded-lg mb-3`}>
+      {Object.keys(course).map((key, i) =>
+        <Item
+          key={key}
+          className={`${i % 2 ? "bg-gray-50" : "bg-white"}`}
+          title={key[0].toUpperCase() + key.slice(1)}
+          value={course[key]}
+        />
+      )}
+      <div className="bg-white px-4 py-5 sm:px-6">
+        {children}
       </div>
     </div>
   )
