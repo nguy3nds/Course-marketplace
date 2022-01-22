0cb0e3a92b2b6d174d76e23a12117a78cb0b2b2d
Sotatek-NguyenNguyen4 <93501310+Sotatek-NguyenNguyen4@users.noreply.github.com>
2022-01-22 09:49:25 +0700

    Use Manage Courses

--- components/providers/web3/hooks/setupHooks.js
+++ components/providers/web3/hooks/setupHooks.js
@@ -1,13 +1,15 @@
 import { handler as createUseAccount } from "./useAccount";
 import { handler as createUseNetwork } from "./useNetwork";
-import { handler as createOwnedCoursesHook } from "./useOwnedCourses"
 import { handler as createOwnedCourseHook } from "./useOwnedCourse"
+import { handler as createOwnedCoursesHook } from "./useOwnedCourses"
+import { handler as createManagedCoursesHook } from "./useManagedCourses"
 
 export const setupHooks = ({ web3, provider, contract }) => {
   return {
     useAccount: createUseAccount(web3, provider),
     useNetwork: createUseNetwork(web3, provider),
+    useOwnedCourse: createOwnedCourseHook(web3, contract),
     useOwnedCourses: createOwnedCoursesHook(web3, contract),
-    useOwnedCourse: createOwnedCourseHook(web3, contract)
+    useManagedCourses: createManagedCoursesHook(web3, contract),
   }
 }
