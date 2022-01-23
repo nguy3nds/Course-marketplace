80c9a80c4ffedeaed70148a3cb65e3623cacf025
Sotatek-NguyenNguyen4 <93501310+Sotatek-NguyenNguyen4@users.noreply.github.com>
2022-01-23 07:39:57 +0700

    Show connect with logout

--- pages/marketplace/index.js
+++ pages/marketplace/index.js
@@ -140,7 +140,10 @@ export default function Marketplace({ courses }) {
                       variant="white"
                       disabled={true}
                       size="sm">
-                      Loading State...
+                      {hasConnectedWallet ?
+                        "Loading State..." :
+                        "Connect"
+                      }
                     </Button>
                   )
                 }
